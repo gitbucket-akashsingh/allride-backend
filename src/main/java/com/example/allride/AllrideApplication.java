173475889a318a39beb3ae64e644f3ecc358e74e
gitbucket-akashsingh <215499269+gitbucket-akashsingh@users.noreply.github.com>
2026-04-14 01:10:00 +0530

    Initial Spring Boot project Setup

---
+++ src/main/java/com/example/allride/AllrideApplication.java
@@ -0,0 +1,13 @@
+package com.example.allride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllrideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllrideApplication.class, args);
+	}
+
+}
